06c8abad2d545d6a74b9e158aadf0b68bc2986bb
victor131999 <tovir.gu.hu@hotmail.com>
2021-07-01 00:39:41 -0500

    Mejora de formulario

--- demomiddleware/resources/views/producto/form.blade.php
+++ demomiddleware/resources/views/producto/form.blade.php
@@ -1,37 +1,39 @@
 <label for="Nombre">Nombre</label>
-    <input type="text" name="Nombre" value="{{$producto->Nombre}}" id="Nombre">
+    <input type="text" name="Nombre" value="{{isset($producto->Nombre)?$producto->Nombre:''}}" id="Nombre">
     <br>
 
     <label for="Precio">Precio</label>
-    <input type="text" name="Precio" value="{{$producto->Precio}}" id="Precio">
+    <input type="text" name="Precio" value="{{isset($producto->Precio)?$producto->Precio:''}}" id="Precio">
     <br>
 
     <label for="Descripcion">Descripción</label>
-    <input type="text" name="Descripcion" value="{{$producto->Descripcion}}" id="Descripcion">
+    <input type="text" name="Descripcion" value="{{isset($producto->Descripcion)?$producto->Descripcion:''}}" id="Descripcion">
     <br>
 
     <label for="Marca">Marca</label>
-    <input type="text" name="Marca" value="{{$producto->Marca}}" id="Marca">
+    <input type="text" name="Marca" value="{{isset($producto->Marca)?$producto->Marca:''}}" id="Marca">
     <br>
 
     <label for="Distribuidora">Distribuidora</label>
-    <input type="text" name="Distribuidora" value="{{$producto->Distribuidora}}" id="Distribuidora">
+    <input type="text" name="Distribuidora" value="{{isset($producto->Distribuidora)?$producto->Distribuidora:''}}" id="Distribuidora">
     <br>
 
     <label for="TipoProducto">Tipo de producto</label>
-    <input type="text" name="TipoProducto" value="{{$producto->TipoProducto}}" id="TipoProducto">
+    <input type="text" name="TipoProducto" value="{{isset($producto->TipoProducto)?$producto->TipoProducto:''}}" id="TipoProducto">
     <br>
 
     <label for="FechaFabricacion">Fecha de Elavoración</label>
-    <input type="text" name="FechaFabricacion" value="{{$producto->FechaFabricacion}}" id="FechFabricacion">
+    <input type="text" name="FechaFabricacion" value="{{isset($producto->FechaFabricacion)?$producto->FechaFabricacion:''}}" id="FechFabricacion">
     <br>
 
     <label for="FechaVencimiento">Fecha de vencimiento</label>
-    <input type="text" name="FechaVencimiento" value="{{$producto->FechaVencimiento}}" id="FechaVencimiento">
+    <input type="text" name="FechaVencimiento" value="{{isset($producto->FechaVencimiento)?$producto->FechaVencimiento:''}}" id="FechaVencimiento">
     <br>
 
     <label for="Foto">Foto</label>
-    <img src="{{asset('storage').'/'.$producto->Foto}}" width="100" alt="">
+    @if(isset($producto->Foto))
+        <img src="{{asset('storage').'/'.$producto->Foto}}" width="100" alt="">
+    @endif
     <input type="file" name="Foto" value="" id="Foto">
     <br>
 
